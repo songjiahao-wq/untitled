511ae294c714e754565e72a5bc9c7b6cbaff39d7
宋家豪 <1109790848@qq.com>
2023-06-11 17:27:50 +0800

    6.11

---
+++ youhua/数据集相关/去除文件名称的空格.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+
+# 设置目标目录
+dir_path = r'D:\my_job\MY_Github\faster-rcnn-pytorch-master\VOCdevkit\VOC2007\Annotations'
+
+# 遍历目录中的文件
+for filename in os.listdir(dir_path):
+    # 仅考虑xml文件
+    if filename.endswith('.xml'):
+        # 创建没有空格的新文件名
+        new_filename = filename.replace(' ', '')
+        # 获取文件的原始和新路径
+        original_path = os.path.join(dir_path, filename)
+        new_path = os.path.join(dir_path, new_filename)
+        # 重命名文件
+        shutil.move(original_path, new_path)
