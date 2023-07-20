99bfdc2f9a3662cfa574e12af12cfcc346c6e60d
songjiahao-wq <1109790848@qq.com>
2023-07-20 17:22:31 +0800

    add Work

--- Attention/hall_conv.py
+++ Attention/hall_conv.py
@@ -7,8 +7,9 @@ class half_conv(nn.Module):
         super(half_conv, self).__init__()
         self.c_ = c1 // 2
         self.conv3x3 = nn.Conv2d(self.c_, self.c_, 3, 1, 3//2)
-        self.conv1x1 = nn.Conv2d(c1, self.c_, 1, 1)
+        self.conv1x1 = DWConv(c1, self.c_, 1, 1)
         self.convout = nn.Conv2d(self.c_, self.c_, 3, 2, 3//2)
+        self.Conv = Conv(c1, c2, k, s)
     def forward(self, x):
         y = torch.split(x, [self.c_, self.c_], dim=1)
         identity1 = y[1]
