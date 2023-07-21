c614cb8da0baf72f1ae1e6f3bf6539d719471f95
songjiahao-wq <1109790848@qq.com>
2023-07-21 14:29:29 +0800

    add Work

--- Attention/Conv.py
+++ Attention/Conv.py
@@ -1,9 +1,12 @@
 import torch.nn as nn
 import torch
+import math
 __all__ = "Conv"
 
-def autopad(k, p=None):  # kernel, padding
-    # Pad to 'same'
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    # Pad to 'same' shape outputs
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
     if p is None:
         p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
     return p
--- Attention/hall_conv.py
+++ Attention/hall_conv.py
@@ -1,15 +1,16 @@
 import torch
 import torch.nn as nn
 from Attention import Conv,DWConv
+from thop import clever_format, profile
 
 class half_conv(nn.Module):
-    def __init__(self,c1, c2, k=1, s=1):
+    def __init__(self, c1, c2, k=1, s=1):
         super(half_conv, self).__init__()
         self.c_ = c1 // 2
-        self.conv3x3 = nn.Conv2d(self.c_, self.c_, 3, 1, 3//2)
-        self.conv1x1 = DWConv(c1, self.c_, 1, 1)
-        self.convout = nn.Conv2d(self.c_, self.c_, 3, 2, 3//2)
-        self.Conv = Conv(c1, c2, k, s)
+        self.conv3x3 = DWConv(self.c_, self.c_, 3, 1, 3//2)
+        self.conv1x1 = Conv(c1, self.c_, 1, 1)
+        self.convout = DWConv(self.c_, self.c_, 3, 2, 3//2)
+        self.Conv = DWConv(c1, c2, k, s)
     def forward(self, x):
         y = torch.split(x, [self.c_, self.c_], dim=1)
         identity1 = y[1]
@@ -19,6 +20,32 @@ class half_conv(nn.Module):
         halfout = self.conv1x1(out)
 
         return torch.cat((halfout, identity2), 1)
+class half_conv_2(nn.Module):
+    def __init__(self, c1, c2, k=1, s=1):
+        super().__init__()
+        self.c_ = c1 // 2
+        self.conv3x3 = DWConv(self.c_, self.c_, 3, 1, 3//2)
+        self.conv1x1 = Conv(c1, self.c_, 1, 1)
+        self.convout = DWConv(c1, self.c_, 3, 1, 3//2)
+        self.Conv = DWConv(c1, c2, k, s)
+    def forward(self, x):
+        y = torch.split(x, [self.c_, self.c_], dim=1)
+        identity1 = y[1]
+        identity2 = y[0]
+        y1 =self.conv3x3(y[0])
+        out = torch.cat((y1, identity1), dim=1)
+        halfout = self.convout(out)
+
+        return torch.cat((halfout, identity2), 1)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+dummy_input = torch.randn(1, 256, 40, 40).to(device)
+for model_name in [half_conv,half_conv_2]:
+    model = model_name(256, 256, 3, 1)
+    flops, params = profile(model.to(device), (dummy_input,), verbose=False)
+    flops, params = clever_format([flops * 2, params], "%.3f")
+    print('Total FLOPS: %s' % (flops))
+    print('Total params: %s' % (params))
 
 if __name__ == "__main__":
     input = torch.randn(1, 256, 40, 40)
