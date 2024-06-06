3d3528a4752b9c2ffe3fd7fb2b34ef2f2b0f42c0
宋家豪 <1109790848@qq.com>
2024-06-06 23:01:51 +0800

    1

--- youhua/数据集相关/去除文件名称的空格.py
+++ youhua/数据集相关/去除文件名称的空格.py
@@ -2,12 +2,12 @@ import os
 import shutil
 
 # 设置目标目录
-dir_path = r'D:\project\7.4\MY-YOLOv5-master\add\voc2yolo\dataset\VOCdevkit\Annotations'
+dir_path = r'D:\xian_yu\xianyu_project\su ke da\DATA\labels'
 
 # 遍历目录中的文件
 for filename in os.listdir(dir_path):
     # 仅考虑xml文件
-    if filename.endswith('.xml') or filename.endswith('.jpg'):
+    if filename.endswith('.xml') or filename.endswith('.jpg') or filename.endswith('.txt'):
         # 创建没有空格的新文件名
         new_filename = filename.replace(' ', '')
         # 获取文件的原始和新路径
